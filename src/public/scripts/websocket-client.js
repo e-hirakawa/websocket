9fee25a4c9870aaf3cacadea8c77cd243b87f25c
Eiji Hirakawa <e.hirakawa@gmail.com>
2019-01-06 15:53:40 +0900

    add websocket-event-type

--- src/public/scripts/websocket-client.js
+++ src/public/scripts/websocket-client.js
@@ -1,18 +1,4 @@
 /**
- * WebSocketイベント種別
- */
-const WebSocketEventType = {
-    /** 接続 */
-    OPENED: 'OPENED',
-    /** 切断 */
-    CLOSED: 'CLOSED',
-    /** 障害 */
-    FAILED: 'FAILED',
-    /** 受信 */
-    RECIEVED: 'RECIEVED'
-}
-
-/**
  * Socketイベント割り当てクラス
  */
 class WebSocketEvents {
